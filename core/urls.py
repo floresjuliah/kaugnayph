0016e9811e72547e02306dc239a66ddc002ce4b1
Maria Allyana <mariaallyana.villafuerte@benilde.edu.ph>
2026-05-20 16:33:09 +0000

    Updated register page and urls

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('dashboard/admin/',    views.admin_dashboard_view,     name='admin_dashboard'),
     path('dashboard/resident/', views.resident_dashboard_view,  name='resident_dashboard'),
     path('pending/',            views.pending_verification_view, name='pending_verification'),
+    path('admin-login/', views.admin_login, name='admin_login'),
+    path('admin-register/', views.admin_register, name='admin_register'),
+
+    
 ]
