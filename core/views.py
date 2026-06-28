f8cbc3e609eeeeb5600985a48221b8a3d74b5c82
Juliah Flores <juliahdflores@gmail.com>
2026-06-28 09:50:05 +0000

    fixed announcement

--- core/views.py
+++ core/views.py
@@ -296,18 +296,11 @@ def documents(request):
 def faqs(request):
     faqs = FAQs.objects.filter(is_active=True)
 
-    cutoff = timezone.make_aware(datetime(2026, 6, 28, 16, 0))
-
-    announcements = Announcements.objects.filter(
-        created_at__gte=cutoff
-    ).order_by("-created_at")
-
     return render(
         request,
-        "faqs.html",
+        'faqs.html',
         {
-            "faqs": faqs,
-            "announcements": announcements,
+            'faqs': faqs
         }
     )
 
